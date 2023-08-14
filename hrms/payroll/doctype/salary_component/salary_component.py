e2f834f22bfda26266e8768ae9da8339485884de
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2023-08-14 06:16:46 +0300

    Sync to target version

---
+++ hrms/payroll/doctype/salary_component/salary_component.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.model.naming import append_number_if_name_exists
+
+
+class SalaryComponent(Document):
+	def validate(self):
+		self.validate_abbr()
+
+	def on_update(self):
+		self.invalidate_cache()
+
+	def validate_abbr(self):
+		if not self.salary_component_abbr:
+			self.salary_component_abbr = "".join([c[0] for c in self.salary_component.split()]).upper()
+
+		self.salary_component_abbr = self.salary_component_abbr.strip()
+		self.salary_component_abbr = append_number_if_name_exists(
+			"Salary Component",
+			self.salary_component_abbr,
+			"salary_component_abbr",
+			separator="_",
+			filters={"name": ["!=", self.name]},
+		)
+
+	def invalidate_cache(self):
+		frappe.cache().delete_value("tax_components")
